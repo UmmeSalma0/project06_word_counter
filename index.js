02fa3ada5c64b712bfb2a7a0e08017a389fe4d81
UmmeSalma0 <UmmeSalma000@outlook.com>
2024-06-26 14:47:16 +0500

    push currency converter to github

---
+++ index.js
@@ -0,0 +1,14 @@
+#!usr/bin/env node                 // shebang for the terminal to run the code.
+import inquirer from 'inquirer'; // import the inquirer module which is a command line interface for Node js
+const answers = await inquirer.prompt([
+    {
+        name: "sentence",
+        type: "input",
+        message: "Enter the sentence to count the words:",
+    }
+]);
+const words = answers.sentence.trim().split(" ");
+// trim() is a method that removes whitespaces from the starts and ends of string.
+// split() is a method that splits/segregates/separates the string into an array of substrings.
+console.log(`There are ${words.length} words in the sentence`); // print the number of words in the sentence
+console.log(words); // print the word in the form of an array in console
